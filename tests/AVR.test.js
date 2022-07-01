6b4d659aada5b6ba5f6501635bfc8e5935ab6f92
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2022-07-01 19:01:20 +0200

    basic testing

---
+++ tests/AVR.test.js
@@ -0,0 +1,18 @@
+const { AVR } = require("../lib/AVR");
+
+test("AVR.test.js", async () => {
+  expect(
+    new AVR({
+      port: "/dev/tty.usbmodem1411",
+    }).buildBaseCommand()
+  ).toBe("avrdude -p m328p -c arduino -P /dev/tty.usbmodem1411 -b 19200");
+
+  expect(
+    new AVR({
+      port: "/dev/tty.usbmodem1412",
+      baudrate: "9600",
+      device: "atmega328p",
+      programmer: "stk500v1",
+    }).buildBaseCommand()
+  ).toBe("avrdude -p atmega328p -c stk500v1 -P /dev/tty.usbmodem1412 -b 9600");
+});
