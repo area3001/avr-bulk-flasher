03684acfd998c463b69e7abe093fd33268d1665e
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2022-07-06 22:03:08 +0200

    write signature

--- index.js
+++ index.js
@@ -17,55 +17,60 @@ const argv = require("minimist")(process.argv.slice(2));
   }
 
   console.log(`Using port(s): ${ports.join(", ")}`);
-  while (true) {
-    for (const port of ports) {
-      const avr = new AVR({
-        port,
-        baudrate: argv.baudrate || 19200,
-        device: argv.device || "atmega328p",
-        programmer: argv.programmer || "stk500v1",
-      });
-      console.log(`Flashing ${file} on ${port}`);
-      try {
-        const signature = await avr.readSignature();
-        console.log(
-          `Found a signature ${signature}, waiting a second to write fuses`
-        );
-        await sleep(1000);
-        console.log(`Fuses written, waiting a second to flash ${file}`);
-        await sleep(1000);
-        await avr.flash(file);
-        console.log("Waiting for device to reset");
-        while (true) {
-          await sleep(1000);
-          try {
-            await avr.readSignature();
-            console.log("Device has not reset");
-          } catch (error) {
-            console.log("Device reset");
-            break;
-          }
-        }
-      } catch (error) {
-        console.error(error);
-      }
-      // waiting for device to reset
-      while (true) {
-        await sleep(1000);
-        try {
-          await avr.readSignature();
-          console.log("Device ready");
-        } catch (error) {
-          console.log("Device not ready");
-          break;
-        }
-      }
-    }
+  const { baudrate, device, programmer } = argv;
+  for (const port of ports) {
+    const avr = new AVR({
+      port,
+      baudrate,
+      device,
+      programmer,
+    });
+    flashLoop(port, file, avr);
   }
 })().catch((error) => {
   console.error(`Error: ${error.message}`);
   process.exit(1);
 });
+
+async function flashLoop(port, file, avr) {
+  while (true) {
+    console.log(`Flashing ${file} on ${port}`);
+    try {
+      const signature = await avr.readSignature();
+      console.log(
+        `Found a signature ${signature}, waiting a second to write fuses`
+      );
+      await sleep(100);
+      await avr.writeFuses();
+      await sleep(100);
+      await avr.flash(file);
+      console.log("Waiting for device to reset");
+      while (true) {
+        await sleep(100);
+        try {
+          await avr.readSignature();
+          console.log("Device has not reset");
+        } catch (error) {
+          console.log("Device reset");
+          break;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    // waiting for device to reset
+    while (true) {
+      await sleep(100);
+      try {
+        await avr.readSignature();
+        console.log("Device ready");
+      } catch (error) {
+        console.log("Device not ready");
+        break;
+      }
+    }
+  }
+}
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
